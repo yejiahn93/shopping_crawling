18d6f16cf5c61b6adf5c2bf567b204879814e0b9
yejiahn93 <yejiahn93@gmail.com>
2022-03-08 14:25:02 -0800

    crawling1

---
+++ inf_scroll.py
@@ -0,0 +1,44 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+
+browser = webdriver.Chrome('/Users/yejiahn/Downloads/chromedriver')
+
+browser.get('https://shopping.google.com/?nord=1')
+
+
+#browser.find_element_by_css_selector('a.nav.shop').click
+
+#search click
+
+search = browser.find_element_by_css_selector('input.r7gAOb.yyJm8b')
+search.click()
+
+#search item
+search.send_keys('iphone 13 promax')
+search.send_keys(Keys.ENTER)
+
+#infinity scroll
+before_h = browser.execute_script("return window.scrollY")
+while True:
+    browser.find_element_by_css_selector("body").send_keys(Keys.END)
+
+    time.sleep(2)
+
+    after_h = browser.execute_script("return window.scrollY")
+    if after_h == before_h:
+        break
+
+    before_h = after_h
+
+items = browser.find_elements_by_css_selector(".KZmu8e")
+
+for item in items:
+    name = item.find_element_by_css_selector(".sh-np__product-title.translate-content").text
+    try:
+        price = item.find_element_by_css_selector(".hn9kf").text
+    except:
+        price = "NA"
+    label = item.find_element_by_css_selector(".sh-np__seller-container").text
+    print(name, price, label)
